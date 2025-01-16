733db25ae417f8508f505867d61bb5812d925078
King Soorsa <kingsoorsa@gmail.com>
2025-01-16 21:49:38 +0100

    .

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,1 +1,13 @@
+import Signinform from '@/Components/Formmeterial/Signinform'
+import React from 'react'
 
+const page = () => {
+  return (
+    <div  className=' flex items-center justify-center '>
+     
+   <Signinform />
+       </div>
+  )
+}
+
+export default page
