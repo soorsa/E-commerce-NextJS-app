0dfad83cd97f26878d5ab99be1647a9de6b9dab8
King Soorsa <kingsoorsa@gmail.com>
2025-01-16 21:41:51 +0100

    .

--- src/app/Search/[searchid]/page.tsx
+++ src/app/Search/[searchid]/page.tsx
@@ -1,1 +1,12 @@
+import React from 'react'
 
+const page = ({params}:any) => {
+  const text = decodeURIComponent(params.searchid)
+  return (
+    <div className='text-orange-600 text-3xl h-[82vh] flex justify-center items-center bg-gray-300' >
+<p className='text-orange-500'>Here is no data of {text}</p>
+    </div>
+  )
+}
+
+export default page
