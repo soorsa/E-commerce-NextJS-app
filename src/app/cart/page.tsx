f13a6b588229a62dfaeb977b3c69e6554a13ed7b
King Soorsa <kingsoorsa@gmail.com>
2025-01-16 21:47:03 +0100

    .

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,1 +1,20 @@
+import Cartitemsection from '@/Components/Cart/Cartitemsection'
+import Cartsummery from '@/Components/Cart/Cartsummery/Cartsummery'
+import React from 'react'
 
+const page = () => {
+  return (
+    <div className='p-5'>
+        <h1 className='text-3xl text-center text-black capitalize  py-8'>Your cart  [2 item]</h1>
+        <div className="tableofrm overflow-x-auto w-[1200px] m-auto">
+          <Cartitemsection/>
+       <div className="c-summery flex justify-end">
+       <Cartsummery />
+       </div>
+   
+        </div>
+        </div>
+  )
+}
+
+export default page
