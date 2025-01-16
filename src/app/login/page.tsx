b995fe7b607854d026d3f69612d31d6c8b19fe79
King Soorsa <kingsoorsa@gmail.com>
2025-01-16 21:45:25 +0100

    .

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,1 +1,12 @@
+import Loginform from '@/Components/Formmeterial/Loginform'
 
+
+const page = () => {
+  return (
+    <div className='h-[82vh] flex items-center justify-center '>
+        <Loginform />
+    </div>
+  )
+}
+
+export default page
