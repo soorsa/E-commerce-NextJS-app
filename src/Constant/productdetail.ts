753a21f85c89a2ff296817b90a9d8d53ed95e03b
King Soorsa <kingsoorsa@gmail.com>
2025-01-16 21:52:37 +0100

    .

---
+++ src/Constant/productdetail.ts
@@ -0,0 +1,77 @@
+const productsdetail = [
+    {
+        "id": 1,
+        "name": "Wireless Bluetooth Headphones",
+        "price": 49.99,
+        "category": "Electronics",
+        "description": "Noise-cancelling, over-ear Bluetooth headphones with a 20-hour battery life.",
+        "in_stock": true,
+        "color":"red",
+        "rating": 4.5,
+        "size":"xl",
+        "main_image": "https://images.unsplash.com/photo-1448582649076-3981753123b5?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8YmFnfGVufDB8fDB8fHww",
+        "image2": "https://images.unsplash.com/photo-1512201078372-9c6b2a0d528a?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGJhZ3xlbnwwfHwwfHx8MA%3D%3D",
+        "image3": "https://images.unsplash.com/photo-1535120927584-0230f40fc1e2?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzF8fGJhZ3xlbnwwfHwwfHx8MA%3D%3D",
+        "image4": "https://images.unsplash.com/photo-1506152983158-b4a74a01c721?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGJhZ3xlbnwwfHwwfHx8MA%3D%3D",
+        "image5": "https://images.unsplash.com/photo-1484527273420-c598cb0601f8?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGJhZ3xlbnwwfHwwfHx8MA%3D%3D",
+        "red-image":"https://images.unsplash.com/photo-1506152983158-b4a74a01c721?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGJhZ3xlbnwwfHwwfHx8MA%3D%3D"
+    },
+    {
+        "id": 2,
+        "name": "Stainless Steel Water Bottle",
+        "price": 19.99,
+        "category": "Home & Kitchen",
+        "description": "Insulated water bottle that keeps drinks cold for 24 hours or hot for 12 hours.",
+        "in_stock": true,
+        "rating": 4.7,
+        "main_image": "https://plus.unsplash.com/premium_photo-1672829371359-e1ef1e6ae381?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzd8fGJhZ3xlbnwwfHwwfHx8MA%3D%3D",
+        "image2": "https://images.unsplash.com/photo-1535120927584-0230f40fc1e2?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzF8fGJhZ3xlbnwwfHwwfHx8MA%3D%3D",
+        "image3": "https://images.unsplash.com/photo-1448582649076-3981753123b5?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8YmFnfGVufDB8fDB8fHww",
+        "image4": "https://images.unsplash.com/photo-1512201078372-9c6b2a0d528a?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGJhZ3xlbnwwfHwwfHx8MA%3D%3D",
+        "image5": "https://images.unsplash.com/photo-1506152983158-b4a74a01c721?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGJhZ3xlbnwwfHwwfHx8MA%3D%3D"
+    },
+    {
+        "id": 3,
+        "name": "Gaming Mouse",
+        "price": 34.99,
+        "category": "Electronics",
+        "description": "Ergonomic gaming mouse with customizable RGB lighting and 7 programmable buttons.",
+        "in_stock": false,
+        "rating": 4.3,
+        "main_image": "https://images.unsplash.com/photo-1448582649076-3981753123b5?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8YmFnfGVufDB8fDB8fHww",
+        "image2": "https://plus.unsplash.com/premium_photo-1672829371359-e1ef1e6ae381?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzd8fGJhZ3xlbnwwfHwwfHx8MA%3D%3D",
+        "image3": "https://images.unsplash.com/photo-1535120927584-0230f40fc1e2?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzF8fGJhZ3xlbnwwfHwwfHx8MA%3D%3D",
+        "image4": "https://images.unsplash.com/photo-1512201078372-9c6b2a0d528a?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGJhZ3xlbnwwfHwwfHx8MA%3D%3D",
+        "image5": "https://images.unsplash.com/photo-1506152983158-b4a74a01c721?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGJhZ3xlbnwwfHwwfHx8MA%3D%3D"
+    },
+    {
+        "id": 4,
+        "name": "Running Shoes",
+        "price": 79.99,
+        "category": "Footwear",
+        "description": "Lightweight running shoes with breathable mesh upper and cushioned sole.",
+        "in_stock": true,
+        "rating": 4.6,
+        "main_image": "https://images.unsplash.com/photo-1484527273420-c598cb0601f8?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGJhZ3xlbnwwfHwwfHx8MA%3D%3D",
+        "image2": "https://plus.unsplash.com/premium_photo-1672829371359-e1ef1e6ae381?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzd8fGJhZ3xlbnwwfHwwfHx8MA%3D%3D",
+        "image3": "https://images.unsplash.com/photo-1448582649076-3981753123b5?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8YmFnfGVufDB8fDB8fHww",
+        "image4": "https://images.unsplash.com/photo-1512201078372-9c6b2a0d528a?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGJhZ3xlbnwwfHwwfHx8MA%3D%3D",
+        "image5": "https://images.unsplash.com/photo-1535120927584-0230f40fc1e2?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzF8fGJhZ3xlbnwwfHwwfHx8MA%3D%3D"
+    },
+    {
+        "id": 5,
+        "name": "Electric Toothbrush",
+        "price": 59.99,
+        "category": "Personal Care",
+        "description": "Rechargeable electric toothbrush with 3 brushing modes and a 2-minute timer.",
+        "in_stock": true,
+        "rating": 4.8,
+        "main_image": "https://plus.unsplash.com/premium_photo-1672829371359-e1ef1e6ae381?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzd8fGJhZ3xlbnwwfHwwfHx8MA%3D%3D",
+        "image2": "https://images.unsplash.com/photo-1448582649076-3981753123b5?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8YmFnfGVufDB8fDB8fHww",
+        "image3": "https://images.unsplash.com/photo-1535120927584-0230f40fc1e2?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzF8fGJhZ3xlbnwwfHwwfHx8MA%3D%3D",
+        "image4": "https://images.unsplash.com/photo-1512201078372-9c6b2a0d528a?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGJhZ3xlbnwwfHwwfHx8MA%3D%3D",
+        "image5": "https://images.unsplash.com/photo-1506152983158-b4a74a01c721?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGJhZ3xlbnwwfHwwfHx8MA%3D%3D"
+    }
+]
+
+export default productsdetail;
