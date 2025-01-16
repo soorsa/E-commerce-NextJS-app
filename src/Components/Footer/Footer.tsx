f09431a413478a49c02cf22fd0c0fb220deb4480
King Soorsa <kingsoorsa@gmail.com>
2025-01-16 22:17:20 +0100

    .

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,1 +1,10 @@
 
+const Footer = () => {
+  return (
+<footer className="p-4 bg-orange-400">
+    <p>&copy; 2024 All Right Reseved by Ecccomerce Website</p>
+</footer>
+  )
+}
+
+export default Footer
